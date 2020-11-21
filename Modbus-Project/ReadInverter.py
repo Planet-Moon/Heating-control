83563d7d695f9382635c13c0e41a8a020560faac
Planet-Moon <topwastaken@abwesend.de>
2020-11-21 16:41:40 +0100

    [refractor] move IP Init of inverterClass to func

--- Modbus-Project/ReadInverter.py
+++ Modbus-Project/ReadInverter.py
@@ -29,14 +29,6 @@ class sma_EnergyMeter:
 
 class sma_Inverter:
     def __init__(self, ipAddress):
-        self.ipAddress = ipAddress
-        self.client = ModbusClient(self.ipAddress)
-        try:
-            self.client.connect()
-            print("Connected!")
-        except:
-            print("Connection failed!")
-            exit()
         self.serialnumber = 0
         self.susyID = 0
         self.UnitID = 1
@@ -47,45 +39,67 @@ class sma_Inverter:
         self.timeZone = 0
         self.error = 0
 
-        self.UnitID_Register = modbus_register(self.client,42109,4,1)
-        self.MACAddress_Register = modbus_register(self.client,40497,1,self.UnitID)
-        self.Model_Register = modbus_register(self.client,30053,2,self.UnitID)
-        self.operationHealth_Register = modbus_register(self.client,30201,2,self.UnitID)
-        self.totalPower_Register = modbus_register(self.client,30775,2,self.UnitID)
-        self.todayEnergy_Register = modbus_register(self.client,30535,2,self.UnitID)
-        self.timeZone_Register = modbus_register(self.client,40003,2,self.UnitID)
+        self._change_IP(ipAddress)
 
         try: 
             self.get_data("all")
         except Exception as e:
-            print(str(current_time())+": Initialisation of "+self.ipAddress+" failed!, "+str(e))
+    def _connect_IP(self):
+        self.client = ModbusClient(self.ipAddress)
+        try:
+            self.client.connect()
+            print("Connected!")
+        except:
+            print("Connection failed!")
+            exit()
+        self._Init_Modbus_Registers()
+
+    def _Init_Modbus_Registers(self):
+        self.modbus = {}
+        self.modbus["UnitID"] = modbus_register(self.client,42109,4,1)
+        self.modbus["MACAddress"] = modbus_register(self.client,40497,1,self.UnitID)
+        self.modbus["Model"] = modbus_register(self.client,30053,2,self.UnitID)
+        self.modbus["operationHealth"] = modbus_register(self.client,30201,2,self.UnitID)
+        self.modbus["totalPower"] = modbus_register(self.client,30775,2,self.UnitID)
+        self.modbus["todayEnergy"] = modbus_register(self.client,30535,2,self.UnitID)
+        self.modbus["timeZone"] = modbus_register(self.client,40003,2,self.UnitID)
+        pass
+    
+    def _change_IP(self, ipAddress):
+        self.ipAddress = ipAddress
+        self._connect_IP()
 
     def get_data(self,dataName):
         if dataName == "pysicalData" or dataName == "all":
-            UnitID_response = self.UnitID_Register.get_data()
+            UnitID_response = self.modbus["UnitID"].get_data()
             self.serialnumber = UnitID_response[0]*65536 + UnitID_response[1]
             self.susyID = UnitID_response[2]
             self.UnitID = UnitID_response[3]
+            Model_response = self.modbus["Model"].get_data()
             self.Model = word_to_uint32(Model_response)
             if dataName != "all":
                 return UnitID_response
 
         elif dataName == "operationHealth" or dataName == "all":
+            operationHealth_response = self.modbus["operationHealth"].get_data()
             self.operationHealth = word_to_uint32(operationHealth_response)
             if dataName != "all":
                 return self.operationHealth
 
         elif dataName == "totalPower" or dataName == "all":
+            totalPower_response_byte = self.modbus["totalPower"].get_data()
             self.totalPower = word_to_int32(totalPower_response_byte)
             if dataName != "all":
                 return self.totalPower
 
         elif dataName == "todayEnergy" or dataName == "all":
+            todayEnergy_response = self.modbus["todayEnergy"].get_data()
             self.todayEnergy = word_to_uint32(todayEnergy_response)
             if dataName != "all":
                 return self.todayEnergy
 
         elif dataName == "timeZone" or dataName == "all":
+            timeZone_response = self.modbus["timeZone"].get_data()
             self.timeZone = word_to_uint32(timeZone_response)
             if dataName != "all":
                 return self.timeZone
