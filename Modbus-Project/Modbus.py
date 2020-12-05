80226441bf04554d9fcb1428c5e0c243d6595383
Planet-Moon <topwastaken@abwesend.de>
2020-12-05 22:09:54 +0100

    [feature] add write registers

--- Modbus-Project/Modbus.py
+++ Modbus-Project/Modbus.py
@@ -2,18 +2,32 @@ from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 import TypeConversion as TC
 
 class modbus_device(object):
-    def __init__(self, ipAddress):
-        self.client = ModbusClient(ipAddress)
+    def __init__(self, ipAddress, port=""):
+        self.ipAddress = ipAddress
+        self.port = port
+        if self.port:
+            self.client = ModbusClient(self.ipAddress, port=self.port)
+        else:
+            self.client = ModbusClient(self.ipAddress)
         self.UnitID = 1
-        try:
-            self.client.connect()
-            print("Connected!")
-        except:
-            print("Connection failed!")
-            exit()
-
+        self.connect()
         self.register = {}
         pass
+
+    def connect(self):
+        try:
+            self.client.connect()
+            self.connected = True
+        except:
+            print("Connection to {}:{} failed!".format(self.ipAddress, self.port))
+            exit()
+
+    def close(self):
+        try:
+            self.client.close()
+            self.connected = False
+        except:
+            print("Connection could not be closed!")
 
     def newRegister(self, name, address, length, signed=False, unit=""):
         self.register[name] = self.modbus_register(address, length, signed, unit)
@@ -32,6 +46,13 @@ class modbus_device(object):
             return value
         except:
             print("Error reading value from register "+name) 
+
+    def write_register(self, name, value):
+        try:
+            # value = TC.number_to_list(value)
+            self.register[name].write(self.client, value, self.UnitID)
+        except:
+            print("Error writing register "+name+" with value "+str(value)) 
 
     def read_string(self, name):
         value = self.read_value(name)
@@ -59,13 +80,14 @@ class modbus_device(object):
             self.signed = signed
             self.unit = unit
 
-        def read(self,client, unitID):
+        def read(self, client, unitID):
             self.error = 0
             try:
                 self.response = client.read_holding_registers(self.address,count=self.length, unit=unitID)
             except Exception as e:
                 self.error = 1
                 print("Error reading "+str(client)+", "+str(e))
+            assert(not self.response.isError())
             return self.response
 
         def get_data(self, client, unitID):
@@ -76,3 +98,9 @@ class modbus_device(object):
             except:
                 print("Error reading "+str(client.host)+", register "+str(self.address))
             pass
+
+        def write(self, client, value, unitID):
+            if not len(value) > self.length:
+                rq = client.write_registers(self.address, value, unit=unitID)
+            else:
+                raise Exception("Value too long for register. length = "+str(self.length)) 
