2a55a8614f7aefcbbcebdc7367bdcb230e789ba9
Planet-Moon <topwastaken@abwesend.de>
2021-01-25 21:11:50 +0100

    [doc] add docstrings

--- Modbus-Project/Modbus.py
+++ Modbus-Project/Modbus.py
@@ -3,6 +3,13 @@ import TypeConversion as TC
 
 class modbus_device(object):
     def __init__(self, ipAddress: str, port="", unitID=1):
+        """Create a modbus device
+
+        Args:
+            ipAddress (str): ip address of the modbus device
+            port (str, optional): Port used by TCP. Defaults to "".
+            unitID (int, optional): UnitID to communicate with device. Defaults to 1.
+        """
         self.ipAddress = ipAddress
         self.port = port
         if self.port:
@@ -15,6 +22,8 @@ class modbus_device(object):
         pass
 
     def connect(self):
+        """Connect to modbus device
+        """
         try:
             self.client.connect()
             self.connected = True
@@ -23,6 +32,8 @@ class modbus_device(object):
             exit()
 
     def close(self):
+        """Close connection to device
+        """
         try:
             self.client.close()
             self.connected = False
@@ -30,6 +41,20 @@ class modbus_device(object):
             print("Connection could not be closed!")
 
     def newRegister(self, name: str, address: int, length: int, signed=False, factor=1, type_="int", unit=""):
+        """Create new register
+
+        Args:
+            name (str): Name of the register (will be used to write and read the register)
+            address (int): Address of register
+            length (int): Wordlength of the register
+            signed (bool, optional): True if register value is a signed integer. Defaults to False.
+            factor (float, optional): Factor to calculate value from register data. Defaults to 1.
+            type_ (str, optional): Datatype of the register. Possible types: int, float, bool. Defaults to "int".
+            unit (str, optional): Unit string of the value, e. g. " Wh" or " °C". Defaults to "".
+
+        Returns:
+            [type]: [description]
+        """
         self.register[name] = self.modbus_register(address, length, signed, factor, type_, unit)
         test = self.read(name) # Init values
         if test:
@@ -38,15 +63,36 @@ class modbus_device(object):
             del self.register[name]
 
     def removeRegister(self, name: str):
+        """Delete register from register dictionary
+
+        Args:
+            name (str): Name of the register to remove
+        """
         del self.register[name]
 
     def read(self, name: str):
+        """Read raw data from a modbus register
+
+        Args:
+            name (str): Name of the register
+
+        Returns:
+            int: Data read from the register
+        """
         try:
             return self.register[name].get_data(self.client, self.UnitID)
         except:
             print("Error reading register "+name)
 
     def read_value(self, name: str):
+        """Read the value from a modbus register
+
+        Args:
+            name (str): Name of the register
+
+        Returns:
+            float/int/bool: Value of the register. Datatype is specified in the register.
+        """
         try:
             value = round(float(TC.list_to_number(self.read(name), signed=self.register[name].signed) * self.register[name].factor), 2)
         except Exception as e:
@@ -66,12 +112,27 @@ class modbus_device(object):
         return self.register[name].value
 
     def write_register(self, name: str, value: int):
+        """Write data to modbus register
+
+        Args:
+            name (str): Name of the register
+            value (int): Data to be written
+        """
+        register = self.register[name]
         try:
-            self.register[name].write(self.client, value, self.UnitID)
+            register.write(self.client, value, self.UnitID)
         except:
             print("Error writing register "+name+" with value "+str(value)) 
 
     def read_string(self, name: str):
+        """Read the value from a modbus register with name and unit string
+
+        Args:
+            name (str): Name of the register
+
+        Returns:
+            str: String with Name + value + unit
+        """
         value = self.read_value(name)
         unit = self.register[name].unit
         if not unit:
@@ -81,6 +142,11 @@ class modbus_device(object):
         return string
 
     def read_all(self):
+        """Read all modbus registers
+
+        Returns:
+            list: List of all values. [[Name, value], ... ]
+        """
         ret_val = []
         for i in self.register:
             if self.register[i].unit :
@@ -93,6 +159,16 @@ class modbus_device(object):
 
     class modbus_register:
         def __init__(self, address: int, length: int, signed: bool, factor: float, type_: str, unit: str):
+            """Create modbus register
+
+            Args:
+                address (int): Address of the register
+                length (int): Wordlength of the register
+                signed (bool): True if the register is signed
+                factor (float): Factor of the register data
+                type_ (str): Datatype of the register data
+                unit (str): Unit of the register value
+            """
             self.address = address
             self.length = length
             self.response = []
@@ -105,6 +181,15 @@ class modbus_device(object):
             self.value = None
 
         def read(self, client, unitID: int):
+            """Read the register
+
+            Args:
+                client (modbusClient): Modbusclient of a device
+                unitID (int): UnitID of the device
+
+            Returns:
+                int: Data of the register
+            """
             self.error = 0
             try:
                 self.response = client.read_holding_registers(self.address,count=self.length, unit=unitID)
@@ -115,6 +200,15 @@ class modbus_device(object):
             return self.response
 
         def get_data(self, client, unitID: int):
+            """Read last data of the register and update self.data
+
+            Args:
+                client (modbusClient): Modbusclient of a device
+                unitID (int): UnitID of the device
+
+            Returns:
+                int: Data of the register
+            """
             self.read(client, unitID)
             try: 
                 self.data = self.response.registers
@@ -124,12 +218,23 @@ class modbus_device(object):
             pass
 
         def write(self, client, value: int, unitID: int):
+            """Write data to the register
+
+            Args:
+                client (modbusClient): Modbusclient of a device
+                value (int): Value to be written
+                unitID (int): UnitID of the device
+
+            Raises:
+                Exception: [description]
+            """
             value = int(value / self.factor)
+            value = TC.number_to_wordList(value, self.signed, self.length)
             if isinstance(value, list):
                 if not len(value) > self.length:
                     rq = client.write_registers(self.address, value, unit=unitID)
                 else:
-                    raise Exception("Value too long for register. length = "+str(self.length)) 
+                    raise Exception("Value too long for register. length = "+str(self.length))
             else:
                 rq = client.write_registers(self.address, value, unit=unitID)
             self.value = value
--- Modbus-Project/TypeConversion.py
+++ Modbus-Project/TypeConversion.py
@@ -1,6 +1,16 @@
 from math import floor
 
 def list_to_number(number, length=2, signed=False): #little endian
+    """Convert list of integers to a single integer
+
+    Args:
+        number (list): List to be converted
+        length (int, optional): Wordlength of the list. Defaults to 2.
+        signed (bool, optional): True if data should be signed. Defaults to False.
+
+    Returns:
+        int: Converted list to a single integer
+    """
     unsigned_number = 0
     number_length = len(number)-1
     for i in range(number_length, -1, -1):
@@ -17,16 +27,27 @@ def list_to_number(number, length=2, signed=False): #little endian
             signed_Number = unsigned_number & ~mask
         return signed_Number
 
-def number_to_wordList(number, signed=False):
+def number_to_wordList(number, signed=False, size=1):
+    """Convert list to a wordlist. Splits a integer into multiple 16-bit integers.
+
+    Args:
+        number (int): Large integer to be converted
+        signed (bool, optional): True if data should be signed. Defaults to False.
+        size (int, optional): Length of the output list. Defaults to 1.
+
+    Returns:
+        list: List of 16-bit integers representing the input number
+    """
     numberInternal = number
     needBytes = 0
     testValue = 0
-    returnList = []
+    returnList = [0] * size
     while testValue < number:
         needBytes += 1
         testValue = 2**(16*needBytes)
-        returnList.append(round(numberInternal % (2**16)))
-        numberInternal = numberInternal / (2**16)
+        appendValue = round(numberInternal % (2**16))
+        returnList[needBytes-1] = appendValue
+        numberInternal = int(numberInternal / (2**16))
         pass
     returnList = list(reversed(returnList))
     return returnList
@@ -36,7 +57,7 @@ def main():
     print("testList:"+str(testList))
     testNumber = list_to_number(testList)
     print("testNumber:"+str(testNumber))
-    testList = number_to_wordList(testNumber)
+    testList = number_to_wordList(testNumber, size=4)
     print("testList:"+str(testList))
 
 
